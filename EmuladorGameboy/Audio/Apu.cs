d0c9aa266720d048fc82ebe4795ed0d3185b257e
kauan-matheus <kauan8643@gmail.com>
2026-06-08 17:20:37 -0300

    Initial: starting basic structure

---
+++ EmuladorGameboy/Audio/Apu.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmuladorGameboy.Audio;
+
+internal class Apu
+{
+}
---
+++ EmuladorGameboy/Core/Cpu.cs
@@ -0,0 +1,5 @@
+﻿namespace EmuladorGameboy.Core;
+
+internal class Cpu
+{
+}
